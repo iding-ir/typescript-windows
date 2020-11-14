e8359e04972e77c527132aa7049ccb6e5d84ca51
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-11-14 20:50:21 +0330

    v0.5.1

--- dist/components/Box.d.ts
+++ dist/components/Box.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="react" />
+import "./Box.scss";
 interface BoxProps {
     text: string;
 }
--- dist/components/Box.js
+++ dist/components/Box.js
@@ -7,13 +7,17 @@ exports.default = void 0;
 
 var _react = _interopRequireDefault(require("react"));
 
+require("./Box.scss");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 var Box = function Box(props) {
   var text = props.text;
-  return /*#__PURE__*/_react.default.createElement("div", null, text);
+  return /*#__PURE__*/_react.default.createElement("div", {
+    className: "box"
+  }, text);
 };
 
 var _default = Box;
 exports.default = _default;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jb21wb25lbnRzL0JveC50c3giXSwibmFtZXMiOlsiQm94IiwicHJvcHMiLCJ0ZXh0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7Ozs7QUFNQSxJQUFNQSxHQUFHLEdBQUcsU0FBTkEsR0FBTSxDQUFDQyxLQUFELEVBQXFCO0FBQUEsTUFDdkJDLElBRHVCLEdBQ2RELEtBRGMsQ0FDdkJDLElBRHVCO0FBRy9CLHNCQUFPLDBDQUFNQSxJQUFOLENBQVA7QUFDRCxDQUpEOztlQU1lRixHIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gXCJyZWFjdFwiO1xyXG5cclxuaW50ZXJmYWNlIEJveFByb3BzIHtcclxuICB0ZXh0OiBzdHJpbmc7XHJcbn1cclxuXHJcbmNvbnN0IEJveCA9IChwcm9wczogQm94UHJvcHMpID0+IHtcclxuICBjb25zdCB7IHRleHQgfSA9IHByb3BzO1xyXG5cclxuICByZXR1cm4gPGRpdj57dGV4dH08L2Rpdj47XHJcbn07XHJcblxyXG5leHBvcnQgZGVmYXVsdCBCb3g7XHJcbiJdfQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL3NyYy9jb21wb25lbnRzL0JveC50c3giXSwibmFtZXMiOlsiQm94IiwicHJvcHMiLCJ0ZXh0Il0sIm1hcHBpbmdzIjoiOzs7Ozs7O0FBQUE7O0FBRUE7Ozs7QUFNQSxJQUFNQSxHQUFHLEdBQUcsU0FBTkEsR0FBTSxDQUFDQyxLQUFELEVBQXFCO0FBQUEsTUFDdkJDLElBRHVCLEdBQ2RELEtBRGMsQ0FDdkJDLElBRHVCO0FBRy9CLHNCQUFPO0FBQUssSUFBQSxTQUFTLEVBQUM7QUFBZixLQUFzQkEsSUFBdEIsQ0FBUDtBQUNELENBSkQ7O2VBTWVGLEciLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgUmVhY3QgZnJvbSBcInJlYWN0XCI7XHJcblxyXG5pbXBvcnQgXCIuL0JveC5zY3NzXCI7XHJcblxyXG5pbnRlcmZhY2UgQm94UHJvcHMge1xyXG4gIHRleHQ6IHN0cmluZztcclxufVxyXG5cclxuY29uc3QgQm94ID0gKHByb3BzOiBCb3hQcm9wcykgPT4ge1xyXG4gIGNvbnN0IHsgdGV4dCB9ID0gcHJvcHM7XHJcblxyXG4gIHJldHVybiA8ZGl2IGNsYXNzTmFtZT1cImJveFwiPnt0ZXh0fTwvZGl2PjtcclxufTtcclxuXHJcbmV4cG9ydCBkZWZhdWx0IEJveDtcclxuIl19
