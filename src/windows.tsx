a48c2db38cb4d96e88f67d1fec65995fe44132cf
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-11-29 16:49:09 +0330

    Add more examples

--- src/windows.tsx
+++ src/windows.tsx
@@ -8,30 +8,33 @@ export const windows: Window[] = [
     title: <div>Window 1</div>,
     content: (
       <div style={{ padding: "1rem" }}>
-        Draggable, resizable, minimizable and maximizable. Bounded to the
-        top-left of screen.
+        Draggable, resizable, minimizable and maximizable.
       </div>
     ),
     grids: { x: 0, y: 0, w: 4, h: 4 },
-    bounds: { left: 0, top: 0, right: 6, bottom: 6 },
-    minSize: { w: 200, h: 100 },
-    maxSize: { w: 600, h: 300 },
     draggable: true,
     resizable: true,
     minimizable: true,
     maximizable: true,
-    startMinimized: false,
   },
   {
     key: "window2",
     title: <div>Window 2</div>,
     content: (
-      <div style={{ padding: "1rem" }}>Only draggable. Has no bounds.</div>
+      <div style={{ padding: "1rem" }}>Bounded to the top of the screen.</div>
     ),
-    grids: { x: 4, y: 8, w: 4, h: 4 },
-    minSize: { w: 200, h: 100 },
-    maxSize: { w: 600, h: 300 },
-    bounds: { left: 0, top: 0, right: 12, bottom: 12 },
+    grids: { x: 4, y: 0, w: 4, h: 4 },
+    bounds: { left: 0, top: 0, right: 12, bottom: 6 },
+    draggable: true,
+    resizable: true,
+    minimizable: true,
+    maximizable: true,
+  },
+  {
+    key: "window3",
+    title: <div>Window 3</div>,
+    content: <div style={{ padding: "1rem" }}>Only draggable.</div>,
+    grids: { x: 8, y: 0, w: 4, h: 4 },
     draggable: true,
     resizable: false,
     minimizable: false,
@@ -39,17 +42,27 @@ export const windows: Window[] = [
     startMinimized: false,
   },
   {
-    key: "window3",
-    title: <div>Window 3</div>,
+    key: "window4",
+    title: <div>Window 4</div>,
     content: (
       <div style={{ padding: "1rem" }}>
-        Starting minimized. Bounded to the right side of screen
+        With minimum and maximum window size.
       </div>
     ),
-    grids: { x: 8, y: 0, w: 4, h: 4 },
-    bounds: { left: 6, top: 0, right: 12, bottom: 12 },
+    grids: { x: 0, y: 4, w: 4, h: 4 },
     minSize: { w: 200, h: 100 },
-    maxSize: { w: 600, h: 300 },
+    maxSize: { w: 500, h: 300 },
+    draggable: true,
+    resizable: true,
+    minimizable: true,
+    maximizable: true,
+    startMinimized: false,
+  },
+  {
+    key: "window5",
+    title: <div>Window 5</div>,
+    content: <div style={{ padding: "1rem" }}>Starting minimized.</div>,
+    grids: { x: 4, y: 4, w: 4, h: 4 },
     draggable: true,
     resizable: true,
     minimizable: true,
