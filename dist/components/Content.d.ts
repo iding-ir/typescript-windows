1119378539ed4243623eade31c2b5a4239150e32
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-11-29 16:53:23 +0330

    v2.3.0

--- dist/components/Content.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface ContentProps {
-    i18n: string;
-}
-declare const Content: (props: ContentProps) => JSX.Element;
-export default Content;
-//# sourceMappingURL=Content.d.ts.map
--- dist/components/Title.d.ts
+++
@@ -1,7 +0,0 @@
-/// <reference types="react" />
-interface TitleProps {
-    i18n: string;
-}
-declare const Title: (props: TitleProps) => JSX.Element;
-export default Title;
-//# sourceMappingURL=Title.d.ts.map
