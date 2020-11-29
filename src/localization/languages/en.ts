66bf3865508e29775f0aba830b0189e4d599ac31
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2020-11-29 16:02:40 +0330

    Remove Title and Content components

--- src/localization/languages/en.ts
+++ src/localization/languages/en.ts
@@ -1,15 +1,15 @@
 const i18n = {
   window1: {
     title: "Window 1",
-    content: "Draggable, resizable, minimizable and maximizable.",
+    content: "Draggable, resizable, minimizable and maximizable. Bounded to the top-left of screen.",
   },
   window2: {
     title: "Window 2",
-    content: "Only draggable.",
+    content: "Only draggable. Has no bounds.",
   },
   window3: {
     title: "Window 3",
-    content: "Starting minimized.",
+    content: "Starting minimized. Bounded to the right side of screen",
   },
 };
 
--- src/windows.tsx
+++ src/windows.tsx
@@ -1,14 +1,17 @@
 import React from "react";
 
 import { Window } from "./components/Windows";
-import Content from "./components/Content";
-import Title from "./components/Title";
 
 export const windows: Window[] = [
   {
     key: "window1",
-    title: <Title i18n="window1.title" />,
-    content: <Content i18n="window1.content" />,
+    title: <div>Window 1</div>,
+    content: (
+      <div style={{ padding: "1rem" }}>
+        Draggable, resizable, minimizable and maximizable. Bounded to the
+        top-left of screen.
+      </div>
+    ),
     grids: { x: 0, y: 0, w: 4, h: 3 },
     bounds: { left: 0, top: 0, right: 6, bottom: 6 },
     minSize: { w: 200, h: 100 },
@@ -21,8 +24,10 @@ export const windows: Window[] = [
   },
   {
     key: "window2",
-    title: <Title i18n="window2.title" />,
-    content: <Content i18n="window2.content" />,
+    title: <div>Window 2</div>,
+    content: (
+      <div style={{ padding: "1rem" }}>Only draggable. Has no bounds.</div>
+    ),
     grids: { x: 4, y: 9, w: 4, h: 3 },
     minSize: { w: 200, h: 100 },
     maxSize: { w: 600, h: 300 },
@@ -35,8 +40,12 @@ export const windows: Window[] = [
   },
   {
     key: "window3",
-    title: <Title i18n="window3.title" />,
-    content: <Content i18n="window3.content" />,
+    title: <div>Window 3</div>,
+    content: (
+      <div style={{ padding: "1rem" }}>
+        Starting minimized. Bounded to the right side of screen
+      </div>
+    ),
     grids: { x: 8, y: 0, w: 4, h: 3 },
     bounds: { left: 6, top: 0, right: 12, bottom: 12 },
     minSize: { w: 200, h: 100 },
