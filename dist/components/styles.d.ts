d7cde3b62d72387052146f828145b9ad3e85e3b1
Aydin Ghane Kh <aydin.ghane.kh@gmail.com>
2021-01-22 12:28:39 +0330

    v6.2.0

--- dist/components/styles.d.ts
+++ dist/components/styles.d.ts
@@ -1,3 +1,4 @@
 import { Styles } from "./index.d";
-export declare const useStyles: (styles: Partial<Styles> | undefined) => (data?: unknown) => Record<"tw-windows", string>;
+import { TaskbarLocation } from "./index.d";
+export declare const useStyles: (styles: Partial<Styles> | undefined, taskbarLocation: TaskbarLocation) => (data?: unknown) => Record<"tw-windows", string>;
 //# sourceMappingURL=styles.d.ts.map
